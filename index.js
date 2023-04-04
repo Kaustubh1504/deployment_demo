929a5524a21a9ce7bfd5d63e0e6eadb6ae92518e
Kaustubh0415 <103656121+Kaustubh0415@users.noreply.github.com>
2023-04-04 17:35:42 +0530

    python modules installed

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express")
-
+const {spawn}=require("child_process")
 app=express()
 
 app.get('/',(req,res)=>{
-    res.status(201).json("app on the cloud :)")
+    // res.status(201).json("app on the cloud :)")
+    let dataCompare
+    const childPython = spawn('python',['test.py'])
+
+    childPython.stdout.on('data',(bufferData)=>{
+        // console.log(bufferData);
+        const bufferString = Buffer.from(bufferData).toString();
+        dataCompare = JSON.parse(bufferString)
+        console.log(dataCompare);
+    })
+    childPython.stderr.on('data',(data)=>{
+        console.log(`stderr:${data}`);
+        res.send(data);
+    })
+    childPython.on('close',(code)=>{
+        console.log(`child process exicited with code : ${code}`)
+        res.send(dataCompare);
+        
+    })
+
 })
 app.listen(3000,()=>{
     console.log("started on port 3000")
